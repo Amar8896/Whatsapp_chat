e588aa76561fde165c00f8d83f1eb336ba40adea
AmarN._ <51088828+Amar8896@users.noreply.github.com>
2023-05-28 22:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ if authenticator_status :
 
         st.sidebar.write(
             'Welcome to the homepage of our B.Tech final year project! We are thrilled to present an innovative and impactful project that showcases our expertise and dedication to the field of technology.')
+        st.sidebar(st.markdown("The next word is <span style='color:red'>red</span>",unsafe_allow_html=True))
     if selected == 'Project':
         st.sidebar.image("WCA-bg.png")
         st.sidebar.title("Whatsapp Chat Analyzer")
